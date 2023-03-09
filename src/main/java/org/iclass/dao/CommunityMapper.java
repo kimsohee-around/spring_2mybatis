a3f82712766e36c3c08080f3faeac4583ecfa51b
kimsewhee <koreait.sec2020@gmail.com>
2023-03-09 11:01:44 +0900

    10_mapper 인터페이스 동작설정

--- src/main/java/org/iclass/dao/CommunityMapper.java
+++ src/main/java/org/iclass/dao/CommunityMapper.java
@@ -1,5 +1,22 @@
 package org.iclass.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.iclass.vo.Community;
+import org.springframework.stereotype.Component;
+
+
+@Mapper					// 이 애노테이션이 붙은 인터페이스를 프록시가 구현체 만들어 줍니다.
 public interface CommunityMapper {
 
+	int insert(Community vo);
+	int update(Community vo);
+	int delete(long idx);
+	Community selectByIdx(long idx);
+	int count();
+	long setReadCount();
+	long commentsCount(long mref);
+	int maxOf();
+	List<Community> list();
 }
